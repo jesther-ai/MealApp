f92b96787c7545219bfbee3786a6cde9740a4075
jesther-ai <jesthersilvestre@yahoo.com>
2021-07-24 10:16:39 +0800

    added Features

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
-import 'categories_screen.dart';
+import '../screens/categories_screen.dart';
+import '../screens/category_meals_screen.dart';
+import '../screens/meal_detail_screen.dart';
+import '../screens/tabs_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,11 +11,37 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'DeliMeals',
+      debugShowCheckedModeBanner: false,
+      title: 'Padala-Foods',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              bodyText1: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              bodyText2: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              headline6: TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+                fontWeight: FontWeight.bold,
+              ),
+            ),
       ),
-      home: CategoriesScreen(),
+      routes: {
+        '/': (context) => TabsScreen(),
+        CategoryMealsScreen.routeName: (context) => CategoryMealsScreen(),
+        MealDetailScreen.routeName: (context) => MealDetailScreen(),
+      },
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(builder: (context) {
+          return CategoriesScreen();
+        });
+      },
     );
   }
 }
